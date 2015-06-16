e6d1785fb16a2ef38cba24261bddac90cbabed52
Amporn Sonthi <aomboy30@gmail.com>
2015-06-17 00:17:33 +0700

    test

---
+++ GoogleMap/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GoogleMap
+//
+//  Created by Amporn Sonthi on 4/12/2558 BE.
+//  Copyright (c) 2558 Amporn Sonthi. All rights reserved.
+//
+
+import UIKit
+//import 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
